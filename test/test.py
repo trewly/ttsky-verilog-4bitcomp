efdee63e720ea9a3253ba355681a064ac24b8034
trewlyvuive <nguyenductrung160602@gmail.com>
2026-09-17 17:31:41 -0700

    fix name test

--- test/test.py
+++ test/test.py
@@ -1,40 +1,84 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
-
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
-
+from cocotb.triggers import RisingEdge, ClockCycles, Timer
 
 @cocotb.test()
-async def test_project(dut):
-    dut._log.info("Start")
+async def test_comp(dut):
+ #clock setting
+ clock = Clock(dut.clk,10,units="ns")
+ cocotb.start_soon(clock.start())
 
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
-    cocotb.start_soon(clock.start())
+ #initial reset
+ dut.program_in.value = 0b0000_0000
+ dut.rst_n.value = 0b0
+ dut.start_button.value = 0b0
+ dut.program_mem_write_button.value =0b0
+ dut.read_result_button.value =0b0
 
-    # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
+ await ClockCycles(dut.clk, 2)
+ dut.rst_n.value =0b1
 
-    dut._log.info("Test project behavior")
+ #send first command - PUSH_COMP 1001
+ dut.program_mem_write_button.value =0b0
+ 
+ await RisingEdge(dut.clk)
+ await RisingEdge(dut.clk)
+ dut.program_in.value = 0b1101_1001
+ await RisingEdge(dut.clk)
+ await RisingEdge(dut.clk)
+ dut.program_mem_write_button.value =0b1
+ await ClockCycles(dut.clk, 7)
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+ #send second command - PUSH 0001
+ dut.program_mem_write_button.value =0b0
+ await RisingEdge(dut.clk)
+ await RisingEdge(dut.clk)
+ dut.program_in.value = 0b1110_0001
+ await RisingEdge(dut.clk)
+ await RisingEdge(dut.clk)
+ dut.program_mem_write_button.value =0b1
+ await ClockCycles(dut.clk, 7)
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+ #send third command - ADD 0001
+ dut.program_mem_write_button.value =0b0
+ await RisingEdge(dut.clk)
+ await RisingEdge(dut.clk)
+ dut.program_in.value = 0b1011_0001
+ await RisingEdge(dut.clk)
+ await RisingEdge(dut.clk)
+ dut.program_mem_write_button.value =0b1
+ await ClockCycles(dut.clk, 7)
+ 
+ #send forth command - JMPC 0010
+ dut.program_mem_write_button.value =0b0
+ await RisingEdge(dut.clk)
+ await RisingEdge(dut.clk)
+ dut.program_in.value = 0b0010_0010
+ await RisingEdge(dut.clk)
+ await RisingEdge(dut.clk)
+ dut.program_mem_write_button.value =0b1
+ await ClockCycles(dut.clk, 7)
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+ #send fifth command - PUSH 0001
+ dut.program_mem_write_button.value =0b0
+ await RisingEdge(dut.clk)
+ await RisingEdge(dut.clk)
+ dut.program_in.value = 0b1110_0001
+ await RisingEdge(dut.clk)
+ await RisingEdge(dut.clk)
+ dut.program_mem_write_button.value =0b1
+ await ClockCycles(dut.clk, 7)
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+ #start running
+ dut.start_button.value =0b1
+ await RisingEdge(dut.clk)
+ await RisingEdge(dut.clk)
+ dut.start_button.value =0b0
+
+ #read result
+ await Timer(2000, units="ns")
+ dut.read_result_button.value =0b1
+ await RisingEdge(dut.clk)
+ await RisingEdge(dut.clk)
+ dut.read_result_button.value =0b0
+ await Timer(100, units="ns")
